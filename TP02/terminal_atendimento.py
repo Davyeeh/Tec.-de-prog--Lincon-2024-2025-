41c6b59c9d9d344007e63bd7b5b21fca460b7923
Davi Araújo <ds818860@gmail.com>
2024-12-03 11:54:53 -0300

    update

--- TP02/terminal_atendimento.py
+++ TP02/terminal_atendimento.py
@@ -1,5 +1,6 @@
 from sisbanco import Conta, Banco
 
+
 def terminal():
     sisbanco = Banco()
 
@@ -12,37 +13,50 @@ def terminal():
         print('[3]-Trasnferir')
         print('[4]-Saldo')
         print('[5]-Sair')
-        opcao = input('Digite: ')
+        opcao = int(input('Digite: '))
 
         if opcao == 0:
-            numero = input('Digite o número da conta: ') # Solicite o numero da conta a ser criada
-            conta = Conta(numero) # instacie uma conta com esse número
-            sisbanco.cadastrar(conta) # cadatre a conta no sisbanco
+            # Solicite o numero da conta a ser criada
+            numero = input('Digite o número da conta: ')
+            conta = Conta(numero)  # instacie uma conta com esse número
+            sisbanco.cadastrar(conta)  # cadatre a conta no sisbanco
 
         elif opcao == 1:
-            numero = input('Digite o número da conta: ') # Solicite o numero da conta alvo
-            valor = float(input('Digite o valor a ser creditado: ')) #solicte uma conta com esse numero
-            sisbanco.creditar(numero, valor) #cadastre a conta no sisbanco
+            # Solicite o numero da conta alvo
+            numero = input('Digite o número da conta: ')
+            # solicte uma conta com esse numero
+            valor = float(input('Digite o valor a ser creditado: '))
+            sisbanco.creditar(numero, valor)  # cadastre a conta no sisbanco
 
         elif opcao == 2:
-            numero = input('Digite o número da conta: ') #solicite o numero da conta alvo
-            valor = float(input('Digite o valor a ser debitado: ')) # solicite o valor a ser debitado
-            sisbanco.debitar(numero, valor) #realize a opercao de debito no sisbanco
+            # solicite o numero da conta alvo
+            numero = input('Digite o número da conta: ')
+            # solicite o valor a ser debitado
+            valor = float(input('Digite o valor a ser debitado: '))
+            # realize a opercao de debito no sisbanco
+            sisbanco.debitar(numero, valor)
 
         elif opcao == 3:
-            origem = input('Digite o número da conta de origem: ') #solicite o numero da conta origem
-            destino = input('Digite o número da conta de destino: ')  #solicite o numero da conta destino
-            valor = float(input('Digite o valor a ser transferido: ')) #solicite o valor a ser transferido
-            sisbanco.transferir(origem, destino, valor) #realize a operacao de transferencia no sisbanco
+            # solicite o numero da conta origem
+            origem = input('Digite o número da conta de origem: ')
+            # solicite o numero da conta destino
+            destino = input('Digite o número da conta de destino: ')
+            # solicite o valor a ser transferido
+            valor = float(input('Digite o valor a ser transferido: '))
+            # realize a operacao de transferencia no sisbanco
+            sisbanco.transferir(origem, destino, valor)
 
         elif opcao == 4:
-            numero = input('Digite o número da conta: ') #solicite o numero da conta alvo
-            saldo = sisbanco.saldo(numero) #realize a operacao de obtencao de saldo no sibanco
+            # solicite o numero da conta alvo
+            numero = input('Digite o número da conta: ')
+            # realize a operacao de obtencao de saldo no sibanco
+            saldo = sisbanco.saldo(numero)
             print(f'Saldo: {saldo}')  # exiba o saldo na tela
 
         elif opcao == 5:
             print('SisBanco::Bye!')
             return
+
 
 if __name__ == "__main__":
     terminal()
